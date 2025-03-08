fc13359ec85d32c409fbbcbd962c95ff372f5d84
dicada-lang <im.that.classic.dude@gmail.com>
2025-03-07 20:29:36 -0600

    Update CanShellMD.bat
    0.1 beta indev

--- CanShellMD.bat
+++ CanShellMD.bat
@@ -4,4 +4,4 @@ color 3b
 echo CanShellMD - Lets code something really fun and cool!.
 echo CMD made by https://github.com/dicada-lang/.
 echo see updates or download the lastest version of canshellmd on https://github.com/dicada-lang/CanShellMD (ver: 0.1 beta) .
-pause
+cmd /k
