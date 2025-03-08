6e7ee656402e60d2b5e6507a07c680d18e1a331a
dicada-lang <im.that.classic.dude@gmail.com>
2025-03-07 20:22:07 -0600

    CanShellMD 0.1 beta indev
    the first CSHMD version ever!

---
+++ CanShellMD.bat
@@ -0,0 +1,6 @@
+title CanShellMD
+color 3b
+@echo off
+echo CanShellMD - Lets code something really fun and cool!.
+echo CMD made by https://github.com/dicada-lang/.
+echo see updates or download the lastest version of canshellmd on https://github.com/dicada-lang/CanShellMD (ver: 0.1 beta) .
